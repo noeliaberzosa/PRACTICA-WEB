4914ceed910eba5222118edb42e8eab47e303e99
nicorubira <nicolasdqvas@gmail.com>
2023-12-11 13:14:44 +0100

    Falta arreglar añadir subelemento

--- Practica2/src/router.js
+++ Practica2/src/router.js
@@ -14,12 +14,12 @@ router.get('/', (req, res) => {
     });
 });
 router.post("/new",(req,res)=>{
-    let {nombre, imagen, descripcion, origen, tipo, precio} = req.body;
-    let error = servidor.validar({nombre, imagen, descripcion, origen, tipo, precio});
+    let {nombre, imagen, descripcion, origen, tipo, precio, recetas} = req.body;
+    let error = servidor.validar({nombre, imagen, descripcion, origen, tipo, precio, recetas});
     if (error===0){
-        let id= servidor.addPlato({nombre, imagen, descripcion, origen, tipo, precio});
+        let id= servidor.addPlato({nombre, imagen, descripcion, origen, tipo, precio, recetas});
         res.render('elemento', {
-            plato: servidor.getPlato(id)
+            plato: servidor.getPlato(id),
         });
     }
     else{
@@ -31,7 +31,10 @@ router.post("/new",(req,res)=>{
 });
 router.get('/plato/:id', (req, res) => {
     let plato = servidor.getPlato(req.params.id);
-res.render('elemento', { plato});
+res.render('elemento', { 
+    plato,
+    recetas: plato.recetas
+    });
 });
 
 router.get("/delete/:id",(req,res)=>{
@@ -53,8 +56,8 @@ res.render('formulario_editar', { plato});
 });
 
 router.post('/updated/:id',(req,res)=>{
-    let {nombre, imagen, descripcion, origen, tipo, precio} = req.body;
-    let error = servidor.validar({nombre, imagen, descripcion, origen, tipo, precio});
+    let {nombre, imagen, descripcion, origen, tipo, precio, recetas} = req.body;
+    let error = servidor.validar({nombre, imagen, descripcion, origen, tipo, precio, recetas});
     if(error==0){
         let plato = servidor.getPlato(req.params.id);
         servidor.editarCampos({nombre, imagen, descripcion, origen, tipo, precio},plato);
@@ -67,5 +70,31 @@ router.post('/updated/:id',(req,res)=>{
             })
         }
 });
+
+router.post('/updatedReceta', (req,res)=> {
+    let rec = new servidor.Receta(
+        req.body.nombreR,
+        req.body.usuario,
+        req.body.ingredientes,
+        req.body.imagenR,
+        req.body.personas,
+        req.body.duracion,
+        req.body.pasos,
+        req.body.alergenos,
+        req.body.vegano
+    );
+    let error = servidor.validarRec(rec);
+    if(error == 0){
+        let plato = servidor.getPlato(req.params.id);
+        plato.recetas.push(rec);
+
+        res.render('elemento', {plato})
+
+    }else {
+        res.render('error',{
+            error:error
+        })
+    }
+})
 
 export default router;
